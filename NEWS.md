deab67bf340acbfd8d7f6ebd20b7e4c00514128c
Yuan Tang <terrytangyuan@gmail.com>
2020-03-11 16:30:02 +0000

    version 0.4.9

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+## ggfortify 0.4.9
+
+* This is a minor release of the package with fixes for CRAN check results.
+
 ## ggfortify 0.4.8
 
 * This is a minor release of the package with fixes to support new major release of `survival` (>=3.1-7).
@@ -63,7 +67,7 @@
 * Support `ggbiplot`.
 * More compatible with recent version of `forecast`, `dplyr`, `tidyr`, etc.
 
-# ggfortify 0.1.0
+## ggfortify 0.1.0
 
 * Compatible with ggplot2 v2.0.0.
 * Support `glmnet`.
@@ -71,6 +75,6 @@
 * Support `ggmultiplot` extraction getter / setter.
 * Support `ggmultiplot` arithmetics.
 
-# ggfortify 0.0.4
+## ggfortify 0.0.4
 
 * First release on CRAN.
--- inst/doc/plot_surv.R
+++ inst/doc/plot_surv.R
@@ -1,14 +1,14 @@
-## ----global_options, include=FALSE---------------------------------------
+## ----global_options, include=FALSE--------------------------------------------
 library(knitr)
 opts_chunk$set(fig.width=6, fig.height=3, fig.path='figures/surv-', warning=FALSE)
 
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE---------------------------------------------------------
 library(ggfortify)
 library(survival)
 fit <- survfit(Surv(time, status) ~ sex, data = lung)
 autoplot(fit)
 
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE---------------------------------------------------------
 autoplot(fit, surv.linetype = 'dashed', conf.int = FALSE,
          censor.shape = '*', censor.size = 5, facets = TRUE, ncol = 2)
 
@@ -20,6 +20,6 @@ d.coxph <- survfit(coxph(Surv(time, status) ~ sex, data = lung))
 autoplot(d.coxph, surv.linetype = 'dashed', surv.colour = 'blue',
          conf.int.fill = 'dodgerblue3', conf.int.alpha = 0.5, censor = FALSE)
 
-## ---- fig.width = 8, fig.height = 4--------------------------------------
+## ---- fig.width = 8, fig.height = 4-------------------------------------------
 autoplot(aareg(Surv(time, status) ~ age + sex + ph.ecog, data = lung))
 
